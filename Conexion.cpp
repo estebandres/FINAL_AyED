c51332309d99ea9e8a52ea30725001662ac0f233
Andresteve07 <andresteve07@gmail.com>
2013-09-01 01:18:05 -0300

    Elimine la antigua implementacion de la clase Conexion porque actualmente se encuentra todo en la cabecera

--- Conexion.cpp
+++
@@ -1,8 +0,0 @@
-#include "Conexion.h"
-
-Conexion::Conexion(int * ip_comp_origen, int * ip_comp_destino, int ancho_banda){
-	this->ip_comp_origen=ip_comp_origen;
-	this->ip_comp_destino=ip_comp_destino;
-	this->ancho_banda=ancho_banda;
-}
-
--- Conexion.h
+++ Conexion.h
@@ -1,19 +1,62 @@
 #ifndef CONEXION_H
-#define CCONEXION_H
+#define CONEXION_H
 #include <iostream>
-
+#include <std::set>
 using namespace std;
 
 class Conexion
 {
 	private:
-		Router* router;//Puntero al router donde está conectada la máquina.
-		int* ip;
-		Lista<Pagina> paginas_recibidas;
-		int cant_pag_enviadas;
+		set<int> terminales;
+		int ancho_banda;
+		Cola<Paquete> canal;
 	public:		
-		Conexion(int * ip_comp_origen, int * ip_comp_destino, int ancho_banda);
-		~Conexion();
+		Conexion();
+		//~Conexion();
+		set<int> obtener_terminales();
+		void cargar(Paquete);
+		Paquete leer();
+		int obtener_ancho_banda();
+		bool conexion_saturada();
+		bool conexion_libre();
 };
+
+Conexion::Conexion(){
+}
+
+Conexion::Conexion(int terminal_i, int terminal_j, int ancho){
+	int bornes[]={terminal_i,terminal_j};
+	this->routers=set<int>(bornes,bornes+2);
+	this->ancho_banda=ancho;
+	this->canal.fijar_tamanio_max(this->ancho_banda);
+}
+
+set<int> Conexion::obtener_terminales(){
+	return this->terminales;
+}
+
+void Conexion::cargar(Paquete){
+	//assert(canal.cola_llena() && "Se intenta cargar una conexion que está saturada.");
+	this->canal.encolar(Paquete);
+}
+
+Paquete Conexion::leer(){
+	Paquete paq_leido = this->canal.primer_elemento();
+	this-> canal.desencolar();
+	return paq_leido;
+	
+}
+
+bool Conexion::conexion_saturada(){
+	return canal.cola_llena();
+}
+
+int Conexion::obtener_ancho_banda(){
+	return this->ancho_banda;
+}
+
+bool Conexion::conexion_libre(){
+	return this->canal.es_vacia();
+}
 
 #endif // CONEXION_H
